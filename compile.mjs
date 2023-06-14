5c62096ff7d3008c5872d0a6d7f683b7630c66c9
altriayu <altriayu97@outlook.com>
2023-06-14 14:48:31 +0800

    feat: updateversion

--- compile.mjs
+++ compile.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-const version = "1.0.2"
+const version = "1.0.4"
 
 await $`CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build main.go`
 await $`mv main release/protoc-gen-http-ts-${version}-macos-arm64`
